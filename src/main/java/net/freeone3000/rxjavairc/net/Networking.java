7a6d143d6a9970dbe1d4c55af926b1c5e2192189
James Moore <freeone3000@gmail.com>
2015-07-26 20:48:42 -0400

    Added basis of pull-from-network.

--- src/main/java/net/freeone3000/rxjavairc/net/Networking.java
+++ src/main/java/net/freeone3000/rxjavairc/net/Networking.java
@@ -1,13 +1,57 @@
 package net.freeone3000.rxjavairc.net;
 
 import rx.Observable;
-import rx.Subscription;
+import rx.observables.StringObservable;
 
-/**
- * Created by James on 7/26/2015.
- */
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class Networking {
-	public Observable<IRCMessage> joinServer() {
+	public Observable<IRCMessage> joinServer(InetSocketAddress address, Observable<String> toWrite) {
+		Observable<String> lines = Observable.<String, Socket>using(
+				() -> {
+					Socket socket = new Socket();
+					try {
+						socket.connect(address);
+					} catch(IOException ioe) {
+						throw new RuntimeException(ioe); //will be propagated
+					}
+					return socket;
+				},
+				socket -> {
+					try {
+						OutputStream outStream = socket.getOutputStream();
+						InputStream inStream = socket.getInputStream();
+						toWrite.<String>subscribe(data -> {
+							try {
+								outStream.write(data.getBytes(StandardCharsets.UTF_8));
+							} catch(IOException ioe) {
+								throw new RuntimeException(ioe);
+							}
+						});
 
+						return StringObservable.byLine(StringObservable.decode(StringObservable.from(inStream), StandardCharsets.UTF_8));
+					} catch(IOException ioe) {
+						throw new RuntimeException(ioe);
+					}
+				},
+				socketToDispose -> {
+					try {
+						socketToDispose.close();
+					} catch(IOException ioe) {
+						throw new RuntimeException(ioe); //will be propagated
+					}
+				}
+		);
+
+		return lines.map(this::parseLine);
+	}
+
+	public IRCMessage parseLine(final String line) {
+		return new IRCMessage();
 	}
 }
